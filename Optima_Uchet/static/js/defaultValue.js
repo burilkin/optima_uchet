ad698f9118cb5b893cfc0710415ef27fcd84cd31
burilkin <dburilkin@gmail.com>
2014-01-20 13:30:52 +0300

    Update defaultValue.js
    test fastcgi restart

--- Optima_Uchet/static/js/defaultValue.js
+++ Optima_Uchet/static/js/defaultValue.js
@@ -1,8 +1,8 @@
 var
 sidebarWidth = 200,
 hiddenSidebarWidth = 40,
-hideShowBtn_Hide = "Скрыть",
-hideShowBtn_Show = "Раскрыть",
+hideShowBtn_Hide = "<<<",
+hideShowBtn_Show = ">>>",
 workAreaMainMenuWidth_SidebarShowed = "calc(100% - " + sidebarWidth + "px)",
 workAreaMainMenuWidth_SidebarHidden = "calc(100% - " + hiddenSidebarWidth + "px)",
 workAreaMainMenuTitle_Hidden = "Показать меню",
